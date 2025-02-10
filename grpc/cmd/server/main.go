7fa0f12bf8550203db39187d373100e391bfa246
Yamaguchi Daiki <dyamaguchi@baleen.studio>
2025-02-10 11:41:25 +0900

    feat: grpcでvalidationを処理するよう変更

--- grpc/cmd/server/main.go
+++ grpc/cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/baleen-dyamaguchi/go-grpc-to-connect/grpc/pkg/greetings"
 	"github.com/baleen-dyamaguchi/go-grpc-to-connect/grpc/pkg/logger"
 	greetingsv1 "github.com/baleen-dyamaguchi/go-grpc-to-connect/pkg/gen/proto/greetings/v1"
+	grpc_validator "github.com/grpc-ecosystem/go-grpc-middleware/validator"
 	"google.golang.org/grpc"
 )
 
@@ -19,9 +20,11 @@ func main() {
 	if err != nil {
 		log.Error("failed to listen: %v", "error", err)
 	}
-	var opts []grpc.ServerOption
 
-	grpcServer := grpc.NewServer(opts...)
+	grpcServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			grpc_validator.UnaryServerInterceptor(),
+		))
 	greetingsv1.RegisterGreetingsServiceServer(grpcServer, &greetings.GreetingsServer{})
 	go func() {
 		log.Info("greetings.server is running", "Addr", lis.Addr())
